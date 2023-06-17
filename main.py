43098f06c8d7c2f566ae5531f501734f295e2c8b
Congeladorportatil <41164425+Congeladorportatil@users.noreply.github.com>
2023-06-17 22:10:22 +0200

    Update main.py

--- main.py
+++ main.py
@@ -2,115 +2,115 @@ import tkinter as tk
 import json
 import os
 
-# Definimos que hacer al pulsar el botón de Añadir
+
 def Add():
-  ventanauno.withdraw()
-  ventanados = tk.Tk()
-  ventanados.title("Add info")
-  ventanados.geometry("620x620")
-    # Función para guardar los datos en un archivo JSON
-  labelst = tk.Label(ventanados, text="Página web de donde procede")
+  win1.withdraw()
+  win2 = tk.Tk()
+  win2.title("Add info")
+  win2.geometry("620x620")
+  
+  labelst = tk.Label(win2, text="Type the web/source:")
   labelst.pack()
-  Web = tk.Text(ventanados, width=20, height=1)
+  Web = tk.Text(win2, width=20, height=1)
   Web.pack()
-  labelus = tk.Label(ventanados, text="Escriba su usuario/correo")
+  labelus = tk.Label(win2, text="Type the user:")
   labelus.pack()
-  Usuario = tk.Text(ventanados,width=20 , height=1)
-  Usuario.pack()
-  labelpas = tk.Label(ventanados, text="Escriba su contraseña/credencial")
+  user = tk.Text(win2,width=20 , height=1)
+  user.pack()
+  labelpas = tk.Label(win2, text="Type the password:")
   labelpas.pack()
-  Password = tk.Text(ventanados, width=20, height=1)
+  Password = tk.Text(win2, width=20, height=1)
   Password.pack()
 
-  #Definimos como guardar datos
-  def guardar_datos():
+  def save_data():
     where = Web.get("1.0", tk.END).strip()
-    usuariofin = Usuario.get("1.0", tk.END).strip()
+    userfin = user.get("1.0", tk.END).strip()
     passfin = Password.get("1.0",tk.END).strip()
     if not os.path.exists("datos.json") or os.path.getsize("datos.json") == 0:
-        # El archivo no existe o está vacío, crear una lista vacía
         contenido_json = []
     else:
         with open("datos.json", "r") as a:
             contenido_json = json.load(a)
 
-    datos = {
+    data = {
         "enterprise": where,
-        "usuariofin": usuariofin,
+        "userfin": userfin,
         "passfin": passfin
     }
     
-    contenido_json.append(datos)
+    contenido_json.append(data)
     with open("datos.json", "w") as b:
       json.dump(contenido_json, b)
       
     Web.delete("1.0", tk.END)
-    Usuario.delete("1.0", tk.END)
+    user.delete("1.0", tk.END)
     Password.delete("1.0", tk.END)
     
-  guardar = tk.Button(ventanados, text = "Save", command=guardar_datos)
+  guardar = tk.Button(win2, text = "Save", command=save_data)
   guardar.pack()
   
 
-#Definimos que queremos que ocurra al pulsar el botón de Buscar
 
 def Search():
-  ventanauno.withdraw()
-  ventanasearch = tk.Tk()
-  ventanasearch.title("Search info")
-  ventanasearch.geometry("720x720")
-  labelstb = tk.Label(ventanasearch, text="Escriba aquí el dominio de la web")
+  win1.withdraw()
+  winsearch = tk.Tk()
+  winsearch.title("Search info")
+  winsearch.geometry("720x720")
+  labelstb = tk.Label(winsearch, text="Type the web/source:")
   labelstb.pack()
-  empresa =tk.Text(ventanasearch, width=20, height = 1)
-  empresa.pack()
+  source =tk.Text(winsearch, width=20, height = 1)
+  source.pack()
 
-  def buscar_datos():
+  def search_data():
       if not os.path.exists("datos.json") or os.path.getsize("datos.json") == 0:
-        # El archivo no existe o está vacío, crear una lista vacía
         contenido_json = []
+
       else:
+
         with open("datos.json", "r") as a:
             contenido_json = json.load(a)
           
-      busqueda = empresa.get("1.0", tk.END).strip()
+      search_term = source.get("1.0", tk.END).strip()
       busqueda_encontrada = None
     
-      for datos in contenido_json:
-        if datos["enterprise"] == busqueda:
-          busqueda_encontrada = datos
+      for data in contenido_json:
+        if data["enterprise"] == search_term:
+          tofind_term = data
           break
           
-      busqueda_encontrada_text = tk.Text(ventanasearch, width=80, height=40)
-      busqueda_encontrada_text.pack()
+      havefind_text_text = tk.Text(winsearch, width=80, height=40)
+      
           
           
-      if busqueda_encontrada:
-        texto_encontrar = f"Esto es lo que se ha encontrado con el término buscado: {busqueda}\n"
-        texto_encontrar += f"Cuenta: {datos['usuariofin']}\n"
-        texto_encontrar += f"Contraseña: {datos['passfin']}\n"
+      if tofind_term:
+        havefind_text = f"This is what has been found with the searched term: {search_term}\n"
+        havefind_text += f"Account: {data['userfin']}\n"
+        havefind_text += f"Password: {data['passfin']}\n"
       else:
-        texto_encontrar = "No fue encontrado"
+        havefind_text = "No fue encontrado"
+      
+      havefind_text_text.pack()
+      havefind_text_text.insert("1.0", havefind_text)  
           
-      busqueda_encontrada_text.insert("1.0", texto_encontrar)  
-          
-  buscar = tk.Button(ventanasearch, text = "Search", command=buscar_datos)
-  buscar.pack()
+  searchbutton = tk.Button(winsearch, text = "Search", command=search_data)
+  searchbutton.pack()
 
 # Crear una ventana
-ventanauno = tk.Tk()
+win1 = tk.Tk()
 
 # Configurar propiedades de la ventana
-ventanauno.title("Index. PassKeeper 1.0")
-ventanauno.geometry("620x620")
+win1.title("Index. PassKeeper 1.0")
+win1.geometry("620x620")
 
 # Crear un widget de etiqueta
-label1 = tk.Label(ventanauno, text="Seleccione la opción que le convenga.")
+label1 = tk.Label(win1, text="Select the option that suits you.")
 label1.pack()
 
 # Crear un widget de botón
-A = tk.Button(ventanauno, text="Añade", command=Add)
+A = tk.Button(win1, text="Add", command=Add)
 A.pack()
-B = tk.Button(ventanauno, text="Buscar", command=Search)
+B = tk.Button(win1, text="Search", command=Search)
 B.pack()
+C =tk.Label(win1, text="BETA: limited capabilities")
 # Ejecutar el bucle principal de la aplicación
-ventanauno.mainloop()
+win1.mainloop()
